7f47117c0196e0f7ac0412bf098ee6136f512e88
Neo-X <glen@fracturedplane.com>
2017-02-04 17:08:26 -0800

    Checking that teerainRL scenario still works.

--- ModelEvaluation.py
+++ ModelEvaluation.py
@@ -264,11 +264,11 @@ def simEpoch(actor, exp, model, discount_factor, anchors=None, action_space_cont
             reward_ = actor.actContinuous(exp, action__, bootstrapping=True)
             agent_not_fell = actor.hasNotFallen(exp)
             # print ("performed action: ", reward)
-        
+        """
         if (agent_not_fell == 0):
             print ("Agent fell ", agent_not_fell, " with reward: ", reward_, " from action: ", action)
             # reward_=0
-             
+        """  
         if ((reward_ >= settings['reward_lower_bound'] )):
             discounted_sum = discounted_sum + (((math.pow(discount_factor,state_num) * reward_))) # *(1.0-discount_factor))
         # print ("discounted_sum: ", discounted_sum)
