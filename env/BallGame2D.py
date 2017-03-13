8335c941abb7dcabca62f5c2e74384c743376550
Neo-X <glen@fracturedplane.com>
2017-03-13 16:56:48 -0700

    Updating settings

--- env/BallGame2D.py
+++ env/BallGame2D.py
@@ -67,6 +67,7 @@ class BallGame2D(BallGame1D):
         updates=0
         while ( ( pos[1] >= (-5)) and (not contact)): # object does not fall off map..
         # while ( ( True ) and (not contact)):
+            # print ("Before vel:, ", self.calcVelocity(bootstrapping=bootstrapping))
             contact = self.simulateAction()
             pos = self._obstacle.getPosition()
             pos = (pos[0], pos[1], 0.0)
@@ -84,6 +85,7 @@ class BallGame2D(BallGame1D):
         self._obstacle.setPosition(pos)
         ## The contact seems to be reducing the velocity
         # self._obstacle.setLinearVel((new_vel[0], new_vel[1], 0.0))
+        # print ("Before vel:, ", self.calcVelocity(bootstrapping=bootstrapping))
         avg_vel = vel_sum/updates
         print("avg_vel: ", avg_vel)
         return avg_vel
