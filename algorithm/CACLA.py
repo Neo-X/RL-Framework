a3bf45146543550f6e12d137ecacefd52c8d61d7
Neo-X <glen@fracturedplane.com>
2017-03-30 15:03:17 -0700

    Removing entropy term.

--- algorithm/CACLA.py
+++ algorithm/CACLA.py
@@ -111,16 +111,18 @@ class CACLA(AlgorithmInterface):
         # self._actLoss_drop = (T.sum(0.5 * self._actDiff_drop ** 2)/float(self._batch_size)) # because the number of rows can shrink
         # self._actLoss_drop = (T.mean(0.5 * self._actDiff_drop ** 2))
         ## Computes the distance between actions weighted by the distances between the states that result in those actions
+        """
         state_sum = T.mean(T.pow(self._model.getStateSymbolicVariable(),2), axis=1)
         Distance = ((((state_sum + T.reshape(state_sum, (1,-1)).T) - 2*T.dot(self._model.getStateSymbolicVariable(), self._model.getStateSymbolicVariable().T))))
         action_sum = T.mean(T.pow(self._q_valsActA_drop,2), axis=1)
         Distance_action = ((((action_sum + T.reshape(action_sum, (1,-1)).T) - 2*T.dot(self._q_valsActA_drop, self._q_valsActA_drop.T))))
         weighted_dist = theano.tensor.elemwise.Elemwise(theano.scalar.mul)(Distance, Distance_action)
         self._weighted_mean_dist = T.mean(weighted_dist, axis=1)
-        
+        """
         ## Entropy from A3C, make sure network is not producing same action for everything..
         # self.entropy = -T.mean(T.sum(self._q_valsActA_drop, axis=0))
-        self._weighted_entropy = -T.mean(self._weighted_mean_dist)
+        # self._weighted_entropy = -T.mean(self._weighted_mean_dist)
+        self._weighted_entropy = 0
         
         
         self._actionUpdates = lasagne.updates.rmsprop(self._actLoss + self._actor_regularization + (0.0001 * self._weighted_entropy), 
