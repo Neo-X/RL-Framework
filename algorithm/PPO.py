c3585cee98c8afb587f5fd0a7f5a00c2e1baef76
Neo-X <glen@fracturedplane.com>
2017-05-02 17:11:11 -0700

    Fixes to PPO and settings

--- algorithm/PPO.py
+++ algorithm/PPO.py
@@ -194,9 +194,10 @@ class PPO(AlgorithmInterface):
         # self._actLoss_ = T.mean(self._log_prob) 
         # self._policy_entropy = 0.5 * T.mean(T.log(2 * np.pi * self._q_valsActASTD ) + 1 )
         ## - because update computes gradient DESCENT updates
-        self._actLoss = -1.0 * ((T.mean(self._actLoss_)) + (self._actor_regularization ))
+        # self._actLoss = -1.0 * ((T.mean(self._actLoss_)) + (self._actor_regularization ))
         # self._entropy = -1. * T.sum(T.log(self._q_valsActA + 1e-8) * self._q_valsActA, axis=1, keepdims=True)
         ## - because update computes gradient DESCENT updates
+        self._actLoss = (-1.0 * T.mean(self._actLoss_)) + (1.0 *self._actor_regularization)
         # self._actLoss_drop = (T.sum(0.5 * self._actDiff_drop ** 2)/float(self._batch_size)) # because the number of rows can shrink
         # self._actLoss_drop = (T.mean(0.5 * self._actDiff_drop ** 2))
         self._policy_grad = T.grad(self._actLoss ,  self._actionParams)
