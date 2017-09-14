Fix bug with fd std scaling

algorithm/ForwardDynamics.py
@@ -225,7 +225,7 @@ class ForwardDynamics(AlgorithmInterface):
         action = np.array(norm_action(action, self._action_bounds), dtype=self.getSettings()['float_type'])
         self._model.setStates(state)
         self._model.setActions(action)
-        state_ = scale_state(self._forwardDynamics_std()[0], self._state_bounds)
+        state_ = self._forwardDynamics_std()[0] * (action_bound_std(self._state_bounds))
         return state_
     
     def predict_reward(self, state, action):