2eb1d46de61f56294cc2d6ba7b291dd0b40ffe3a
Neo-X <glen@fracturedplane.com>
2017-09-15 09:49:52 -0700

    Updating models

--- model/DeepNNTanH.py
+++ model/DeepNNTanH.py
@@ -44,11 +44,11 @@ class DeepNNTanH(ModelInterface):
         network = lasagne.layers.DenseLayer(
                 network, num_units=32,
                 nonlinearity=lasagne.nonlinearities.leaky_rectify)
-        """
+        
         network = lasagne.layers.DenseLayer(
                 network, num_units=16,
                 nonlinearity=lasagne.nonlinearities.leaky_rectify)
-        """
+        
         self._critic = lasagne.layers.DenseLayer(
                 network, num_units=1,
                 nonlinearity=lasagne.nonlinearities.linear)
