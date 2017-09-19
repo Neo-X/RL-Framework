524fa4151655a113f114e29463350d0c24f9fef2
Neo-X <glen@fracturedplane.com>
2017-09-19 11:02:19 -0700

    Working on PPO learning

--- model/DeepNNDropoutCritic.py
+++ model/DeepNNDropoutCritic.py
@@ -87,6 +87,11 @@ class DeepNNDropoutCritic(ModelInterface):
                 networkAct, num_units=self._action_length,
                 nonlinearity=lasagne.nonlinearities.linear)
         # self._b_o = init_b_weights((n_out,))
+        if (self._settings['use_stocastic_policy']):
+            with_std = lasagne.layers.DenseLayer(
+                    networkAct, num_units=self._action_length,
+                    nonlinearity=theano.tensor.nnet.softplus)
+            self._actor = lasagne.layers.ConcatLayer([self._actor, with_std], axis=1)
         
         
           # print "Initial W " + str(self._w_o.get_value()) 
