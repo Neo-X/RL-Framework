2f10144d60d67c6392ab8284a8ff2f2662dc502f
Neo-X <glen@fracturedplane.com>
2017-09-28 10:12:27 -0700

    Preparing settings

--- trainMetaModel.py
+++ trainMetaModel.py
@@ -16,5 +16,23 @@ def trainMetaModel(settingsFileName, samples=10):
         trainModelParallel(settingsFileName, settings)
 
 if (__name__ == "__main__"):
+    """
+        python trainMetaModel.py <sim_settings_file> <num_samples>
+        Example:
+        python trainMetaModel.py settings/navGame/PPO_5D.json 10
+    """
     
-    trainMetaModel(sys.argv[1], samples=int(sys.argv[2]))
+    if (len(sys.argv) == 1):
+        print("Please incluse sim settings file")
+        print("python trainMetaModel.py <sim_settings_file> <num_samples>")
+        sys.exit()
+    elif (len(sys.argv) == 2):
+        print("Please incluse sim settings file")
+        print("python trainMetaModel.py <sim_settings_file> <num_samples>")
+        sys.exit()
+    elif (len(sys.argv) == 3):
+        trainMetaModel(sys.argv[1], samples=int(sys.argv[2]))
+    else:
+        print("Please specify arguments properly, ")
+        print(sys.argv)
+        print("python trainMetaModel.py <sim_settings_file> <num_samples>")
