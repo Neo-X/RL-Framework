fd3f9d45a86a1eab7bf395e8d85d297a86ae9a14
Neo-X <glen@fracturedplane.com>
2017-09-28 09:58:23 -0700

    WOring on meta sim

--- trainMetaModel.py
+++ trainMetaModel.py
@@ -4,17 +4,17 @@ from trainModel import trainModelParallel
 import sys
 import json
 
-def trainMetaModel(settingsFileName):
+def trainMetaModel(settingsFileName, samples=10):
     
     file = open(sys.argv[1])
     settings = json.load(file)
     print ("Settings: " + str(json.dumps(settings)))
     file.close()
     data_name = settings['data_folder']
-    for i in range(4):
+    for i in range(10):
         settings['data_folder'] = data_name + "_" + str(i) 
         trainModelParallel(settingsFileName, settings)
 
 if (__name__ == "__main__"):
     
-    trainMetaModel(sys.argv[1])
+    trainMetaModel(sys.argv[1], samples=int(sys.argv[2]))
