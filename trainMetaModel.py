4dd7ce2d2585a3197a93f8d2224e4a0ee65ebeb9
Neo-X <glen@fracturedplane.com>
2017-09-29 16:53:44 -0700

    Fixing meta sim

--- trainMetaModel.py
+++ trainMetaModel.py
@@ -11,8 +11,9 @@ def trainMetaModel(settingsFileName, samples=10):
     print ("Settings: " + str(json.dumps(settings)))
     file.close()
     data_name = settings['data_folder']
-    for i in range(10):
-        settings['data_folder'] = data_name + "_" + str(i) 
+    for i in range(samples):
+        settings['data_folder'] = data_name + "_" + str(i)
+        settings['random_seed'] = int(settings['random_seed']) + ((int(settings['num_available_threads']) + 1) * i) 
         trainModelParallel(settingsFileName, settings)
 
 if (__name__ == "__main__"):
