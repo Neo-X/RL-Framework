1d38aa7b100481a021c8f7fab504057b3f044bdd
Neo-X <glen@fracturedplane.com>
2017-10-03 16:55:24 -0700

    Adjusting threading

--- tuneHyperParameters.py
+++ tuneHyperParameters.py
@@ -66,7 +66,7 @@ def tuneHyperParameters(simsettingsFileName, Hypersettings=None):
     sim_data = []
     for i in range(samples+1):
         param_value = ((range_[1] - range_[0]) * (float(i)/samples)) + range_[0]
-        settings['data_folder'] = data_name + "_" + str(param_value) + "/"
+        settings['data_folder'] = data_name + "_" + param_of_interest + "_"+ str(param_value) + "/"
         settings['action_learning_rate'] = param_value
         directory= getBaseDataDirectory(settings)
         if not os.path.exists(directory):
