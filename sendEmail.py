bff7bb9be415ec52231c303d80ef55b25a88f708
Neo-X <glen@fracturedplane.com>
2017-10-12 13:07:47 -0700

    Working on mail system

--- sendEmail.py
+++ sendEmail.py
@@ -1,5 +1,8 @@
 # Import smtplib for the actual sending function
 import smtplib
+import socket
+import sys
+import getpass
 
 # Import the email modules we'll need
 from email.mime.text import MIMEText
@@ -7,17 +10,32 @@ from email.mime.text import MIMEText
 # Open a plain text file for reading.  For this example, assume that
 # the text file contains only ASCII characters.
 # with open(textfile) as fp:
-def sendEmail(subject, contents):
+def sendEmail(subject, contents, testing=False):
     # Create a text/plain message
     msg = MIMEText(contents)
     
+    hostname = socket.getfqdn()
+    
+    print("Hostname: ", hostname)
+    
+    username = getpass.getuser()
+    print ("username: ", username)
     # me == the sender's email address
     # you == the recipient's email address
     msg['Subject'] = subject
     msg['From'] = 'csguestn@dyn.cs.sfu.ca'
     msg['To'] = 'gberseth@cs.ubc.ca'
     
+    if ( testing ):
+        return
+    
     # Send the message via our own SMTP server.
     s = smtplib.SMTP('localhost')
     s.send_message(msg)
     s.quit()
+    
+
+
+if __name__ == '__main__':
+    
+    sendEmail("Testing", "Nothing", testing=True)
