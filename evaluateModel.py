Fixing advantage computation

evaluateModel.py
@@ -111,10 +111,10 @@ class SimContainer(object):
                 """
                 action_ = np.array(self._agent.predict(state_), dtype='float64')
                 
-                # grad_ = self._agent.getPolicy().getGrads(state_)[0]
-                # self._grad_sum += np.abs(grad_)
+                grad_ = self._agent.getPolicy().getGrads(state_)[0]
+                self._grad_sum += np.abs(grad_)
                 self._num_actions +=1
-                print ("Input grad: ", self._grad_sum/self._num_actions)
+                print ("Input grad: ", repr(self._grad_sum/self._num_actions))
                 
                 
                 # action_[1] = 1.0