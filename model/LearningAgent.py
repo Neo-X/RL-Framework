36f69b95c2d53f5e5ce232b25b8c8115579a16b3
Neo-X <glen@fracturedplane.com>
2017-11-14 10:57:03 -0800

    Adding option to store different buffer for FD model

--- model/LearningAgent.py
+++ model/LearningAgent.py
@@ -188,7 +188,7 @@ class LearningAgent(AgentInterface):
             if (self._settings['train_forward_dynamics']):
                 for i in range(self._settings['critic_updates_per_actor_update']):
                     if ( 'keep_seperate_fd_exp_buffer' in self._settings and (self._settings['keep_seperate_fd_exp_buffer'])):
-                        print ("Using seperate (off-policy) exp mem for FD model")
+                        # print ("Using seperate (off-policy) exp mem for FD model")
                         states__, actions__, result_states__, rewards__, falls__, G_ts__, exp_actions__ = self.getFDExperience().get_batch(value_function_batch_size)
                     else:
                         states__, actions__, result_states__, rewards__, falls__, G_ts__, exp_actions__ = self.getExperience().get_batch(value_function_batch_size)
@@ -239,7 +239,7 @@ class LearningAgent(AgentInterface):
                 dynamicsLoss = 0 
                 if (self._settings['train_forward_dynamics']):
                     if ( 'keep_seperate_fd_exp_buffer' in self._settings and (self._settings['keep_seperate_fd_exp_buffer'])):
-                        print ("Using seperate (off-policy) exp mem for FD model")
+                        # print ("Using seperate (off-policy) exp mem for FD model")
                         _states, _actions, _result_states, _rewards, _falls, _G_ts, _exp_actions = self.getFDExperience().get_batch(value_function_batch_size)
                         
                     dynamicsLoss = self._fd.train(states=_states, actions=_actions, result_states=_result_states, rewards=_rewards)
