635d6c12d2b7a90e4321cef0598f8411a9a1b1d9
Neo-X <glen@fracturedplane.com>
2018-05-12 21:13:57 -0400

    Fixing environment and saving more

--- tests/test_saveandload.py
+++ tests/test_saveandload.py
@@ -10,12 +10,31 @@ import json
 
 class TestSaveAndLoad(object):
 
-    def test_keras_gapGame_2D_save_and_load_singleNet(self):
+    def test_ppo_keras_gapGame_2D_save_and_load_singleNet(self):
         """
             Test that PPO can still learn a good policy on 2d gapgame sim
             If this does not crash, things are good..
         """
         filename = "tests/settings/gapGame2D/PPO/SingleNet_FixedSTD_Tensorflow-v2.json"
+        file = open(filename)
+        settings = json.load(file)
+        file.close()
+        settings['visualize_learning'] = False
+        settings['shouldRender'] = False
+        settings['print_level'] = 'testing_sim'
+        settings['rounds'] = 10
+        simData = trainModelParallel((filename, settings))
+        # assert np.mean(simData['mean_reward'][-5:]) > -0.5
+        settings['visualize_expected_value'] = False
+        modelEvaluation(filename, runLastModel=False, settings=settings, render=False)
+        
+    
+    def test_cacla_keras_gapGame_2D_save_and_load_singleNet(self):
+        """
+            Test that PPO can still learn a good policy on 2d gapgame sim
+            If this does not crash, things are good..
+        """
+        filename = "tests/settings/gapGame2D/CACLA/Net_FixedSTD.json"
         file = open(filename)
         settings = json.load(file)
         file.close()
