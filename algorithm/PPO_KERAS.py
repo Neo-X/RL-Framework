8cb841d08d1f06a4cbc21ecc8364a532c2080f6c
Neo-X <glen@fracturedplane.com>
2018-05-14 21:38:36 -0400

    Working on LLC learning again

--- algorithm/PPO_KERAS.py
+++ algorithm/PPO_KERAS.py
@@ -647,3 +647,42 @@ class PPO_KERAS(KERASAlgorithm):
     def get_critic_loss(self):
         return self._get_critic_loss([])
     
+    def saveTo(self, fileName):
+        # print(self, "saving model")
+        import dill
+        suffix = ".h5"
+        ### Save models
+        # self._model._actor_train.save(fileName+"_actor_train"+suffix, overwrite=True)
+        self._model._actor.save(fileName+"_actor"+suffix, overwrite=True)
+        self._model._critic.save(fileName+"_critic"+suffix, overwrite=True)
+        self._modelTarget._actor.save(fileName+"_actor_T"+suffix, overwrite=True)
+        self._modelTarget._critic.save(fileName+"_critic_T"+suffix, overwrite=True)
+        ### Make a temp copy of models
+        model_actor_train = self._model._actor_train
+        model_actor = self._model._actor
+        model_critic = self._model._critic
+        modelTarget_actor = self._modelTarget._actor
+        modelTarget_critic = self._modelTarget._critic
+        ### Set models to none so they are not saved with this pickling... Because Keras does not pickle well.
+        self._model._actor_train = None
+        self._model._actor = None
+        self._model._critic = None
+        self._modelTarget._actor = None
+        self._modelTarget._critic = None
+        ### Pickle this class
+        """
+        suffix = ".pkl"
+        file_name=fileName+suffix
+        f = open(file_name, 'wb')
+        dill.dump(self, f)
+        f.close()
+        """
+        ### Restore models
+        # self._model = model
+        # self._modelTarget = modelTarget
+        self._model._actor_train = model_actor_train
+        self._model._actor = model_actor
+        self._model._critic = model_critic
+        self._modelTarget._actor = modelTarget_actor
+        self._modelTarget._critic = modelTarget_critic
+        # print ("self._model._actor_train: ", self._model._actor_train)
