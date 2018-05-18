080981a85347ecf0847357e276755944af6dbf57
Neo-X <glen@fracturedplane.com>
2018-05-18 11:52:28 -0400

    Fixed #66, GOt loading to work and added test for it

--- tests/test_saveandload.py
+++ tests/test_saveandload.py
@@ -39,6 +39,35 @@ class TestSaveAndLoad(object):
         assert np.allclose(simData['masterAgent'].getActionBounds(), evalData['masterAgent'].getActionBounds())
         assert np.allclose(simData['masterAgent'].getStateBounds(), evalData['masterAgent'].getStateBounds())
         
+    def test_cacla_tensorflow_gapGame_2D_save_and_load_singleNet(self):
+        """
+            Test that PPO can still learn a good policy on 2d gapgame sim
+            If this does not crash, things are good..
+        """
+        filename = "tests/settings/gapGame2D/CACLA/Net_FixedSTD_Tensorflow.json"
+        file = open(filename)
+        settings = json.load(file)
+        file.close()
+        settings['visualize_learning'] = False
+        settings['shouldRender'] = False
+        settings['print_level'] = 'testing_sim'
+        settings["bootstrap_samples"] = 1000
+        settings['rounds'] = 4
+        settings["epochs"] = 2
+        settings['eval_epochs'] = 2
+        settings["plotting_update_freq_num_rounds"] = 2
+        settings["saving_update_freq_num_rounds"] =  2
+        settings["state_normalization"] = "variance"
+        simData = trainModelParallel((filename, settings))
+        # assert np.mean(simData['mean_reward'][-5:]) > -0.5
+        settings['visualize_expected_value'] = False
+        evalData = modelEvaluation(filename, runLastModel=False, settings=settings, render=False)
+        print ("state bounds: ", simData['masterAgent'].getStateBounds(), evalData['masterAgent'].getStateBounds())
+               
+        assert np.allclose(simData['masterAgent'].getRewardBounds(), evalData['masterAgent'].getRewardBounds())
+        assert np.allclose(simData['masterAgent'].getActionBounds(), evalData['masterAgent'].getActionBounds())
+        assert np.allclose(simData['masterAgent'].getStateBounds(), evalData['masterAgent'].getStateBounds())
+        
     def test_cacla_keras_gapGame_2D_save_and_load_singleNet(self):
         """
             Test that PPO can still learn a good policy on 2d gapgame sim
