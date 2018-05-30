b350367a9d527ca95532e1e99bb79745ff206fc5
Neo-X <glen@fracturedplane.com>
2018-05-30 12:38:40 -0400

    Adding assume yes flag for install

--- tools/installPython3.6.sh
+++ tools/installPython3.6.sh
@@ -5,8 +5,8 @@ sudo add-apt-repository ppa:jonathonf/python-3.6  # (only for 16.04 LTS)
 ## Then, run the following (this works out-of-the-box on 16.10 and 17.04):
 
 sudo apt update
-sudo apt install python3.6
-sudo apt install python3.6-dev
+sudo apt install -y python3.6
+sudo apt install -y python3.6-dev
 # sudo apt install python3.6-venv
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3.6 get-pip.py
