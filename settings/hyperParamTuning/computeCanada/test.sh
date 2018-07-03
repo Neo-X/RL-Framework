af2b4c9762ff01a8283c1e0ef831af154adde8ed
Neo-X <glen@fracturedplane.com>
2018-07-03 16:12:30 -0400

    Working on siamese network design

---
+++ settings/hyperParamTuning/computeCanada/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --account=def-vandepan
+#SBATCH --mem=128000M
+# The arguments should always start with the keyword SBATCH
+# The arguments must immediately follow the first line (i.e. `!/bin/bash`)
+echo 'Hello, world!'  # use your command here
