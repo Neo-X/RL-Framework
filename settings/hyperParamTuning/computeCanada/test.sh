49eaf05f2a43c96d3d6fcce92a2ec9f576217193
Neo-X <glen@fracturedplane.com>
2018-07-03 16:22:02 -0400

    Working on siamese network design

--- settings/hyperParamTuning/computeCanada/test.sh
+++ settings/hyperParamTuning/computeCanada/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --account=rrg-vandepan
 #SBATCH --mem=1024M
-#SBATCH --time=0-0:5
+#SBATCH --time=00:01:00
+#SBATCH --job-name=test
+#SBATCH --output=%x-%j.out
 #SBATCH --cpus-per-task=2
 # The arguments should always start with the keyword SBATCH
 # The arguments must immediately follow the first line (i.e. `!/bin/bash`)
