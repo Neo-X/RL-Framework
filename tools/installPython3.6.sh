1c69b6d96c336661c4001165ffa0c74dd4fdd717
Neo-X <glen@fracturedplane.com>
2018-07-07 12:49:09 -0400

    Working on PPO more...

--- tools/installPython3.6.sh
+++ tools/installPython3.6.sh
@@ -4,6 +4,8 @@ sudo add-apt-repository ppa:jonathonf/python-3.6  # (only for 16.04 LTS)
 
 ## Then, run the following (this works out-of-the-box on 16.10 and 17.04):
 
+user=""
+# user="--user"
 sudo apt update
 sudo apt install -y python3.6
 sudo apt install -y python3.6-dev
@@ -16,23 +18,23 @@ sudo python3.6 get-pip.py
 
 sudo apt-get -y install python3-dev python3-pip
 
-pip3.6 install --user numpy
-pip3.6 install --user theano==1.0.1
+pip3.6 install $user numpy
+pip3.6 install $user theano==1.0.1
 # pip install matplotlib
-pip3.6 install --user matplotlib
-pip3.6 install --user Lasagne==0.1
+pip3.6 install $user matplotlib
+pip3.6 install $user Lasagne==0.1
 ### One of these should work... Curse you Lasagne...
 cp ../patches/pool.py ~/.local/lib/python3.6/site-packages/lasagne/layers/
-pip3.6 install --user dill
-pip3.6 install --user pathos
-pip3.6 install --user pyOpenGL
-# pip3 install --user keras==2.1.5
+pip3.6 install $user dill
+pip3.6 install $user pathos
+pip3.6 install $user pyOpenGL
+# pip3 install $user keras==2.1.5
 ### I have a custom version of keras now...
-pip3.6 install --user git+https://github.com/Neo-X/keras.git
-pip3.6 install --user tensorflow
+pip3.6 install $user git+https://github.com/Neo-X/keras.git
+pip3.6 install $user tensorflow
 ## Does not work in Python3
 # pip3 install pyODE
-pip3.6 install --user h5py
+pip3.6 install $user h5py
 
 sudo apt-get -y install python3-tk
 
