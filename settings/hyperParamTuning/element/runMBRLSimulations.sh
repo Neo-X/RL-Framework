073c19ee54c17a7fe6a6b105a2cb1a305c2d1d38
Neo-X <glen@fracturedplane.com>
2018-07-08 13:25:58 -0400

    Working on more viz learning

--- settings/hyperParamTuning/element/runMBRLSimulations.sh
+++ settings/hyperParamTuning/element/runMBRLSimulations.sh
@@ -7,34 +7,35 @@
 ## declare an array variable
 declare -a metaExps=(
 # 				"settings/hyperParamTuning/element/activation_type.json" 
- 				"settings/hyperParamTuning/element/additional_on-poli_trianing_updates.json" 
+#  				"settings/hyperParamTuning/element/additional_on-poli_trianing_updates.json" 
 # 				"settings/hyperParamTuning/element/anneal_exploration.json" 
-				"settings/hyperParamTuning/element/anneal_learning_rate.json" 		
-				"settings/hyperParamTuning/element/batch_size.json"
+# 				"settings/hyperParamTuning/element/anneal_learning_rate.json" 		
+# 				"settings/hyperParamTuning/element/batch_size.json"
 # 				"settings/hyperParamTuning/element/CACLA_use_advantage.json"
 # 				"settings/hyperParamTuning/element/CACLA_use_advantage_action_weighting.json"
 # 				"settings/hyperParamTuning/element/clamp_actions_to_stay_inside_bounds.json" 
 # 				"settings/hyperParamTuning/element/critic_learning_rate.json"
-				"settings/hyperParamTuning/element/critic_network_layer_sizes.json"
+# 				"settings/hyperParamTuning/element/critic_network_layer_sizes.json"
 # 				"settings/hyperParamTuning/element/critic_updates_per_actor_update.json"
 # 				"settings/hyperParamTuning/element/dont_use_td_learning.json"
 # 				"settings/hyperParamTuning/element/dropout_p.json" 
 				"settings/hyperParamTuning/element/exploration_rate.json" 
 #				"settings/hyperParamTuning/element/GAE_lambda.json"
 # 				"settings/hyperParamTuning/element/initial_temperature.json" 
- 				"settings/hyperParamTuning/element/kl_divergence_threshold.json" 
+#  				"settings/hyperParamTuning/element/kl_divergence_threshold.json" 
 # 				"settings/hyperParamTuning/element/normalize_advantage.json" 
 #  				"settings/hyperParamTuning/element/num_on_policy_rollouts.json" 
 #  				"settings/hyperParamTuning/element/optimizer.json" 
 # 				"settings/hyperParamTuning/element/policy_activation_type.json"
 # 				"settings/hyperParamTuning/element/policy_network_layer_sizes.json"	
- 				"settings/hyperParamTuning/element/ppo_et_factor.json"
+#  				"settings/hyperParamTuning/element/ppo_et_factor.json"
 # 				"settings/hyperParamTuning/element/pretrain_critic.json" 
 #				"settings/hyperParamTuning/element/state_normalization.json"		
 #  				"settings/hyperParamTuning/element/use_single_network.json"
 #  				"settings/hyperParamTuning/element/use_stocastic_policy.json"
 #  				"settings/hyperParamTuning/element/use_target_net_for_critic.json"
 #				"settings/hyperParamTuning/element/value_function_batch_size.json"
+				"settings/hyperParamTuning/element/fd_network_layer_sizes.json"
 )
 
 rounds=$2
@@ -46,7 +47,7 @@ do
 	# or do whatever with individual element of the array
 	# echo "$simConfigFile"
 	
-	command="borgy submit --restartable --req-cores=32 --req-ram-gbytes=32 -w /home/${USER} --image=images.borgy.elementai.lan/glen:latest -e LD_LIBRARY_PATH=/usr/lib/nvidia-390/:/usr/lib/x86_64-linux-gnu/mesa/:/usr/lib/x86_64-linux-gnu/mesa-egl/ -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -- /bin/bash -c 'pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config="$simConfigFile" --metaConfig="$metaConfig" --meta_sim_samples=4 --meta_sim_threads=4 --tuning_threads=1 --num_rounds="$rounds" | tee -a $BORGY_JOB_ID.out'"
+	command="borgy submit --restartable --req-cores=32 --req-ram-gbytes=32 -w /home/${USER} --image=images.borgy.elementai.lan/glen:latest -e LD_LIBRARY_PATH=/usr/lib/nvidia-390/ -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -- /bin/bash -c 'pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config="$simConfigFile" --metaConfig="$metaConfig" --meta_sim_samples=4 --meta_sim_threads=4 --tuning_threads=1 --num_rounds="$rounds" | tee -a $BORGY_JOB_ID.out'"
 	echo $command
 	eval $command
 done
