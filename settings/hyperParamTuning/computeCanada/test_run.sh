3a82c0f1b20d0979f20da2a90a2bdcc421c86cc5
Neo-X <glen@fracturedplane.com>
2018-10-23 13:01:03 -0700

    Working on compute canada stuff

--- settings/hyperParamTuning/computeCanada/test_run.sh
+++ settings/hyperParamTuning/computeCanada/test_run.sh
@@ -5,6 +5,14 @@
 #SBATCH --job-name=test
 #SBATCH --output=%x-%j.out
 #SBATCH --cpus-per-task=8
+
+#SBATCH --mail-user=gberseth@cs.ubc.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
 # The arguments should always start with the keyword SBATCH
 # The arguments must immediately follow the first line (i.e. `!/bin/bash`)
 $1  # use your command here
