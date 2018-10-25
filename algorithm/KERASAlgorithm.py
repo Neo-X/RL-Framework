f5307818a99d75aff3e8221a7d71066df38140df
Neo-X <glen@fracturedplane.com>
2018-10-24 18:43:27 -0700

    Working on new reward function type

--- algorithm/KERASAlgorithm.py
+++ algorithm/KERASAlgorithm.py
@@ -213,7 +213,7 @@ class KERASAlgorithm(AlgorithmInterface):
             # y_ = self._modelTarget.getCriticNetwork().predict(result_states, batch_size=states.shape[0])
             # target_ = rewards + ((self._discount_factor * y_))
             # target_2 = norm_reward(G_t, self.getRewardBounds()) * (1.0-self.getSettings()['discount_factor'])
-            target_2 = G_t 
+            # target_2 = G_t 
             # print ("targets, V, G", np.concatenate((target_, target_2, target_ - target_2), axis=1))
             # print ("Shaping reward", np.concatenate((target_, falls, target_ * falls), axis=1))
             target = G_t
