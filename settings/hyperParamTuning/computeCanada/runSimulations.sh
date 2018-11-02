b1efe7f660dd7d03f0bde5853b5d2c7e373add76
Neo-X <glen@fracturedplane.com>
2018-11-02 09:36:30 -0700

    Working on compute canada simulations

--- settings/hyperParamTuning/computeCanada/runSimulations.sh
+++ settings/hyperParamTuning/computeCanada/runSimulations.sh
@@ -6,37 +6,13 @@
 
 ## declare an array variable
 declare -a metaExps=(
-# 				"settings/hyperParamTuning/element/activation_type.json" 
- 				"settings/hyperParamTuning/element/additional_on-poli_trianing_updates.json" 
-				"settings/hyperParamTuning/element/advantage_scaling.json" 
-# 				"settings/hyperParamTuning/element/anneal_exploration.json" 
-				"settings/hyperParamTuning/element/anneal_learning_rate.json" 		
-				"settings/hyperParamTuning/element/batch_size.json"
-# 				"settings/hyperParamTuning/element/CACLA_use_advantage.json"
-# 				"settings/hyperParamTuning/element/CACLA_use_advantage_action_weighting.json"
-# 				"settings/hyperParamTuning/element/clamp_actions_to_stay_inside_bounds.json" 
-# 				"settings/hyperParamTuning/element/critic_learning_rate.json"
-#				"settings/hyperParamTuning/element/critic_network_layer_sizes.json"
-# 				"settings/hyperParamTuning/element/critic_updates_per_actor_update.json"
-# 				"settings/hyperParamTuning/element/dont_use_td_learning.json"
-# 				"settings/hyperParamTuning/element/dropout_p.json" 
-				"settings/hyperParamTuning/element/exploration_rate.json" 
-#				"settings/hyperParamTuning/element/GAE_lambda.json"
-# 				"settings/hyperParamTuning/element/initial_temperature.json" 
- 				"settings/hyperParamTuning/element/kl_divergence_threshold.json" 
-# 				"settings/hyperParamTuning/element/normalize_advantage.json" 
-#  				"settings/hyperParamTuning/element/num_on_policy_rollouts.json" 
-#  				"settings/hyperParamTuning/element/optimizer.json" 
-# 				"settings/hyperParamTuning/element/policy_activation_type.json"
-# 				"settings/hyperParamTuning/element/policy_network_layer_sizes.json"	
-#  				"settings/hyperParamTuning/element/ppo_et_factor.json"
-# 				"settings/hyperParamTuning/element/pretrain_critic.json" 
- 				"settings/hyperParamTuning/element/reset_on_fall.json" 
-#				"settings/hyperParamTuning/element/state_normalization.json"		
-#  				"settings/hyperParamTuning/element/use_single_network.json"
-#  				"settings/hyperParamTuning/element/use_stocastic_policy.json"
-#  				"settings/hyperParamTuning/element/use_target_net_for_critic.json"
-#				"settings/hyperParamTuning/element/value_function_batch_size.json"
+	"settings/terrainRLMultiChar/HLC/CACLA/LargeBlocks_Multi_Char_On_Policy.json"
+	"settings/terrainRLMultiChar/HLC/CACLA/Dynamic_Obstacles.json"
+	"settings/terrainRLMultiChar/HLC/CACLA/Dynamic_Obstacles_NEWLLC.json"
+	"settings/terrainRLImitate3D/Path_Folowing/Dynamic_Obstacles.json"
+	"settings/terrainRLImitate3D/Path_Folowing/LargeBlocks_OnPolicy-OLDLLC.json"
+	"settings/terrainRLImitate3D/Path_Folowing/LargeBlocks_OnPolicy-NEWLLC.json"
+	"settings/terrainRLImitate3D/Path_Folowing/Path_Following.json"
 )
 
 rounds=$2
@@ -53,9 +29,6 @@ do
 	eval $command
 done
 
-### TerrainRL sim
-# borgy submit --restartable --req-cores=32 --req-ram-gbytes=32 -w /home/${USER} --image=images.borgy.elementai.lan/glen:latest -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -- /bin/bash -c 'pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config=settings/terrainRLImitate3D/PPO/Flat_Tensorflow.json --metaConfig=settings/hyperParamTuning/element/use_single_network.json --meta_sim_samples=4 --meta_sim_threads=4 --tuning_threads=1 --num_rounds=250 | tee -a $BORGY_JOB_ID.out'
-
 ### Toy Sim
-# borgy submit --restartable --req-cores=32 --req-ram-gbytes=32 -w /home/${USER} --image=images.borgy.elementai.lan/glen:latest -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -- /bin/bash -c 'pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config=tests/settings/gapGame2D/PPO/FixedSTD_Tensorflow-v2_dropout.json --metaConfig=settings/hyperParamTuning/element/dropout_p_and_use_dropout_in_actor.json --meta_sim_samples=5 --meta_sim_threads=5 --tuning_threads=2 --num_rounds=250 | tee -a $BORGY_JOB_ID.out'
+# SINGULARITYENV_TERRAINRL_PATH=/opt/TerrainRL SINGULARITYENV_RLSIMENV_PATH=/opt/RLSimulationEnvironments sbatch --time=50:00:00 ./settings/hyperParamTuning/computeCanada/test_run.sh "singularity exec --cleanenv --home /home/gberseth/playground/RL-Framework/:/opt/RL-Framework /scratch/gberseth/playground/singularity/ubuntu_learning.simg python3.6 trainModel.py --config=settings/terrainRLImitate3D/Path_Folowing/Path_Following.json -p 8 --bootstrap_samples=1000 --rollouts=16 --max_epoch_length=64 --on_policy=fast --save_experience_memory=continual --num_rounds=250 --print_level=train --continue_training=false --saving_update_freq_num_rounds=1 --epochs=4 --eval_epochs=8 --plot=false --meta_sim_samples=2 --meta_sim_threads=2 --plotting_update_freq_num_rounds=5 --metaConfig=settings/hyperParamTuning/deepCrowds.json --additional_on-poli_trianing_updates=1 --email_log_data_periodically=true --shouldRender=false"
 
