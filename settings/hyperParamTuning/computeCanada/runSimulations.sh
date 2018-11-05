83decbc84bacaf05a6e8173c97275a679cebda36
Neo-X <glen@fracturedplane.com>
2018-11-05 12:58:17 -0800

    Working on running scripts

--- settings/hyperParamTuning/computeCanada/runSimulations.sh
+++ settings/hyperParamTuning/computeCanada/runSimulations.sh
@@ -16,15 +16,14 @@ declare -a metaExps=(
 )
 
 rounds=$2
-simConfigFile=$1
+metaConfigFile=$1
 ## now loop through the above array
-for metaConfig in "${metaExps[@]}"
+for simConfig in "${metaExps[@]}"
 do
 	# echo "$metaConfig"
 	# or do whatever with individual element of the array
 	# echo "$simConfigFile"
-	
-	command="borgy submit --restartable --req-cores=32 --req-ram-gbytes=32 -w /home/${USER} --image=images.borgy.elementai.lan/glen:latest -e RLSIMENV_PATH=/home/glen/playground/RLSimulationEnvironments -e LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/lib/nvidia-390:~/nvidia-390 -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -- /bin/bash -c 'pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config="$simConfigFile" --metaConfig="$metaConfig" --meta_sim_samples=4 --meta_sim_threads=4 --tuning_threads=1 --num_rounds="$rounds" | tee -a $BORGY_JOB_ID.out'"
+	command="SINGULARITYENV_TERRAINRL_PATH=/opt/TerrainRL SINGULARITYENV_RLSIMENV_PATH=/opt/RLSimulationEnvironments sbatch --time=50:00:00 ./settings/hyperParamTuning/computeCanada/test_run.sh 'singularity exec --cleanenv --home /home/gberseth/playground/RL-Framework/:/opt/RL-Framework /scratch/gberseth/playground/singularity/ubuntu_learning.simg python3.6 trainModel.py --config="$simConfig" -p 8 --bootstrap_samples=1000 --rollouts=16 --max_epoch_length=64 --on_policy=fast --save_experience_memory=continual --num_rounds="$rounds" --print_level=train --continue_training=false --saving_update_freq_num_rounds=1 --epochs=4 --eval_epochs=8 --plot=false --meta_sim_samples=2 --meta_sim_threads=2 --plotting_update_freq_num_rounds=5 --metaConfig=settings/hyperParamTuning/deepCrowds.json --additional_on-poli_trianing_updates=1 --email_log_data_periodically=true --shouldRender=false'"
 	echo $command
 	eval $command
 done
