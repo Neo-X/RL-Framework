a73a47ee396731a443a78dc1d9fc23c619b94166
Neo-X <glen@fracturedplane.com>
2019-01-04 10:08:43 -0800

    Working on viz imitation with multimodal data and cam vel

--- tests/test_viz_imitation.py
+++ tests/test_viz_imitation.py
@@ -95,7 +95,28 @@ class TestVizImitation(object):
         assert not (simData is None)
         
         # @pytest.mark.timeout(600)
-    def test_viz_state_withCamVel_normal_reward(self):
+    
+    def test_viz_state_32x32_LSTM_Siamese(self):
+        """
+        Test that CACLA can still learn a good policy on 2d particle sim
+        """
+        filename = "tests/settings/projectileGame/CACLA/Imitation_Learning_Viz_32x32_1Sub_LSTM_FD_Reward_Encode.json"
+        file = open(filename)
+        settings = json.load(file)
+        file.close()
+        this_function_name = sys._getframe().f_code.co_name
+        settings['data_folder'] = settings['data_folder'] + '/' + this_function_name
+        settings['visualize_learning'] = False
+        # settings['shouldRender'] = False
+        settings['print_level'] = 'testing_sim'
+        settings['rounds'] = 10
+        settings['num_threads'] = 2
+        settings['rollouts'] = 4
+        simData = trainModelParallel((filename, settings))
+        # assert np.mean(simData['mean_reward'][-5:]) > -0.5
+        assert not (simData is None)
+        
+    def test_viz_state_32x32_withCamVel_RNN_Siamese(self):
         """
         Test that CACLA can still learn a good policy on 2d particle sim
         """
@@ -115,9 +136,29 @@ class TestVizImitation(object):
         # assert np.mean(simData['mean_reward'][-5:]) > -0.5
         assert not (simData is None)
         
-    def test_viz_state_encoder_decoder_lstm(self):
+    def test_viz_state_32x32_multiModal_withCamVel_Siamese_lstm(self):
         """
         Test that CACLA can still learn a good policy on 2d particle sim
+        """
+        filename = "settings/projectileGame/CACLA/Imitation_Learning_Imitation_Dense_Reward_LSTM_EncoderDecoder.json"
+        file = open(filename)
+        settings = json.load(file)
+        file.close()
+        this_function_name = sys._getframe().f_code.co_name
+        settings['data_folder'] = settings['data_folder'] + '/' + this_function_name
+        settings['visualize_learning'] = False
+        # settings['shouldRender'] = False
+        settings['print_level'] = 'testing_sim'
+        settings['rounds'] = 10
+        settings['num_threads'] = 2
+        settings['rollouts'] = 4
+        simData = trainModelParallel((filename, settings))
+        # assert np.mean(simData['mean_reward'][-5:]) > -0.5
+        assert not (simData is None)
+        
+    def test_viz_state_32x32_encoder_decoder_lstm(self):
+        """
+            Test that CACLA can still learn a good policy on 2d particle sim
         """
         filename = "settings/projectileGame/CACLA/Imitation_Learning_Imitation_Dense_Reward_LSTM_EncoderDecoder.json"
         file = open(filename)
