0defd5bc4c0b9ea7e3f0131db79bc1a98e782f70
Neo-X <glen@fracturedplane.com>
2019-03-25 13:45:07 -0700

    Adding more exps

--- settings/hyperParamTuning/element/runVizImitationSimulations.sh
+++ settings/hyperParamTuning/element/runVizImitationSimulations.sh
@@ -22,11 +22,12 @@ declare -a metaExps=(
 # 				"settings/hyperParamTuning/element/critic_updates_per_actor_update.json"
 #				"settings/hyperParamTuning/element/dont_use_td_learning.json"
 # 				"settings/hyperParamTuning/element/dropout_p.json" 
-# 				"settings/hyperParamTuning/element/exploration_rate.json"
+ 				"settings/hyperParamTuning/element/exploration_rate.json"
 #				"settings/hyperParamTuning/element/fd_algorithm.json"
 #  				"settings/hyperParamTuning/element/fd_distance_function.json"
-# 				"settings/hyperParamTuning/element/fd_learning_rate.json" 
-#  				"settings/hyperParamTuning/element/fd_updates_per_actor_update.json" 
+ 				"settings/hyperParamTuning/element/fd_expereince_length.json"
+ 				"settings/hyperParamTuning/element/fd_learning_rate.json" 
+  				"settings/hyperParamTuning/element/fd_updates_per_actor_update.json" 
 #				"settings/hyperParamTuning/element/GAE_lambda.json"
 # 				"settings/hyperParamTuning/element/image_noise_scale.json" 
 #  				"settings/hyperParamTuning/element/imperfect_compare_offset.json" 
@@ -44,7 +45,7 @@ declare -a metaExps=(
 # 				"settings/hyperParamTuning/element/pretrain_critic.json" 
 # 				"settings/hyperParamTuning/element/reset_on_fall.json" 
 #				"settings/hyperParamTuning/element/state_normalization.json"
-				"settings/hyperParamTuning/element/use_agent_multitask_data.json"		
+#				"settings/hyperParamTuning/element/use_agent_multitask_data.json"		
 #  				"settings/hyperParamTuning/element/use_single_network.json"
 #  				"settings/hyperParamTuning/element/use_stocastic_policy.json"
 #  				"settings/hyperParamTuning/element/use_target_net_for_critic.json"
@@ -60,8 +61,12 @@ declare -a simConfigs=(
 #	"settings/terrainRLImitate3D/PPO/Imitation_Learning_GRF_Walk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Encode.json"
 #	"settings/terrainRLImitate3D/TRPO/Imitation_Learning_GRF_Walk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Encode.json"
 #
-	"settings/terrainRLImitate3D/TRPO/Imitation_Learning_GRF_MultiTask_Walk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Encode.json"
-	"settings/terrainRLImitate3D/PPO/Imitation_Learning_GRF_MultiTask_Walk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Encode.json"
+#	"settings/terrainRLImitate3D/TRPO/Imitation_Learning_GRF_MultiTask_Walk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Encode.json"
+	"settings/terrainRLImitate3D/TRPO/Imitation_Learning_GRF_Walk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Dual_Encode_2State_2.json"
+	"settings/terrainRLImitate3D/TRPO/Imitation_Learning_GRF_ZombieWalk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Dual_Encode_2State_2.json"
+	"settings/terrainRLImitate3D/TRPO/Imitation_Learning_GRF_Run_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Dual_Encode_2State_2.json"
+
+#	"settings/terrainRLImitate3D/PPO/Imitation_Learning_GRF_MultiTask_Walk_VizWithCamVel_64x64_1Sub_LSTM_FD_Reward_Encode.json"
 )
 
 rounds=$1
@@ -76,11 +81,11 @@ do
 		# or do whatever with individual element of the array
 		# echo "$simConfigFile"
 		output=' | tee -a $BORGY_JOB_ID.out'
-	 	arg="pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config=${simConfigFile} --metaConfig=${metaConfig} --meta_sim_samples=2 --meta_sim_threads=2 --tuning_threads=2 --num_rounds=${rounds} --plot=false --on_policy=fast --save_experience_memory=continual --continue_training=last --saving_update_freq_num_rounds=1 -p 8 --rollouts=16 --simulation_timeout=1200 --email_log_data_periodically=true --Multi_GPU=true --gpus=2 ${opts}"
+	 	arg="pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config=${simConfigFile} --metaConfig=${metaConfig} --meta_sim_samples=2 --meta_sim_threads=2 --tuning_threads=2 --num_rounds=${rounds} --plot=false --on_policy=fast --save_experience_memory=continual --continue_training=last --saving_update_freq_num_rounds=1 -p 6 --rollouts=12 --simulation_timeout=1200 --email_log_data_periodically=true --Multi_GPU=true --gpus=2 ${opts}"
 	### GPU training
 # 	 	arg="source ~/tensorflow/bin/activate; pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config=${simConfigFile} --metaConfig=${metaConfig} --meta_sim_samples=3 --meta_sim_threads=3 --tuning_threads=2 --num_rounds=${rounds} --plot=false --on_policy=fast --shouldRender=false --save_experience_memory=continual --continue_training=last --saving_update_freq_num_rounds=1 -p 4 --rollouts=16 --simulation_timeout=1200 --email_log_data_periodically=true --save_video_to_file=eval_movie2.mp4 --visualize_expected_value=false --force_sim_net_to_cpu=true ${opts}"
 		arg=$arg$output
-		command=(borgy submit --restartable --gpu=2 --cpu=32 --mem=256 --max-run-time-secs=400000 -w /home/"$USER" -v /mnt/home/"$USER":/home/"$USER" --image=images.borgy.elementai.lan/glen:latest -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -e RLSIMENV_PATH=/home/glen/playground/RLSimulationEnvironments -e HOME=/home/"$USER" -e LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/lib/nvidia-390:~/nvidia-390 -- /bin/bash -c "$arg")
+		command=(borgy submit --restartable --gpu=2 --cpu=24 --mem=128 --max-run-time-secs=200000 -w /home/"$USER" -v /mnt/home/"$USER":/home/"$USER" --image=images.borgy.elementai.lan/glen:latest2 -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -e RLSIMENV_PATH=/home/glen/playground/RLSimulationEnvironments -e HOME=/home/"$USER" -- /bin/bash -c "$arg")
 		echo "${command[@]}"
 		# eval $command
 		"${command[@]}"
@@ -92,4 +97,5 @@ done
 
 ### Toy Sim
 # borgy submit --restartable --req-cores=32 --req-ram-gbytes=32 -w /home/${USER} --image=images.borgy.elementai.lan/glen:latest -e TERRAINRL_PATH=/home/glen/playground/TerrainRL/ -- /bin/bash -c 'pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config=tests/settings/gapGame2D/PPO/FixedSTD_Tensorflow-v2_dropout.json --metaConfig=settings/hyperParamTuning/element/dropout_p_and_use_dropout_in_actor.json --meta_sim_samples=5 --meta_sim_threads=5 --tuning_threads=2 --num_rounds=250 | tee -a $BORGY_JOB_ID.out'
+
 
