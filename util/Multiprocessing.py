f2d111f53563380f560abce92c13d0114b4a6348
Neo-X <glen@fracturedplane.com>
2019-06-19 13:08:11 -0700

    Fixing multi processing for python 3.7

--- util/Multiprocessing.py
+++ util/Multiprocessing.py
@@ -19,10 +19,15 @@ class NoDaemonProcess(multiprocessing.Process):
         pass
     daemon = property(_get_daemon, _set_daemon)
 
+class NoDaemonContext(type(multiprocessing.get_context())):
+    Process = NoDaemonProcess
 # We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
 # because the latter is only a wrapper function, not a proper class.
 class MyPool(multiprocessing.pool.Pool):
-    Process = NoDaemonProcess
+    
+    def __init__(self, *args, **kwargs):
+        kwargs['context'] = NoDaemonContext()
+        super(MyPool, self).__init__(*args, **kwargs)
 
 def sleepawhile(t):
     print("Sleeping %i seconds..." % t)
