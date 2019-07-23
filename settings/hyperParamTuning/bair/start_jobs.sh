989e0caa224fb43b43700cc4b65283a5159a5e0a
Neo-X <glen@fracturedplane.com>
2019-07-23 14:55:07 -0700

    Adding new sims

--- settings/hyperParamTuning/bair/start_jobs.sh
+++ settings/hyperParamTuning/bair/start_jobs.sh
@@ -11,7 +11,7 @@
 declare -a metaExps=(
 # 				"settings/hyperParamTuning/element/activation_type.json" 
 #				"settings/hyperParamTuning/element/add_label_noise.json" 
-  				"settings/hyperParamTuning/element/additional_on-poli_trianing_updates.json"
+#  				"settings/hyperParamTuning/element/additional_on-poli_trianing_updates.json"
 # 				"settings/hyperParamTuning/element/advantage_scaling.json" 
 #  				"settings/hyperParamTuning/element/anneal_exploration.json"
 #  				"settings/hyperParamTuning/element/anneal_policy_std.json" 
@@ -23,6 +23,7 @@ declare -a metaExps=(
 # 				"settings/hyperParamTuning/element/critic_updates_per_actor_update.json"
 # 				"settings/hyperParamTuning/element/critic_updates_per_actor_update_trpo.json"
 #				"settings/hyperParamTuning/element/dont_use_td_learning.json"
+				"settings/hyperParamTuning/element/discount_factor.json"
  				"settings/hyperParamTuning/bair/exploration_rate_MARL.json"
 #				"settings/hyperParamTuning/element/GAE_lambda.json"
 # 				"settings/hyperParamTuning/element/initial_temperature.json" 
@@ -43,12 +44,15 @@ declare -a metaExps=(
 declare -a simConfigs=(
 #	"settings/navgame2D/MADDPG/HRL_Tensorflow_NoViz_HLC-v5.json"
 # 	"settings/navgame2D/MADDPG/HRL_Ant_PoseGoal_TRPO_Tensorflow_NoViz_HLC-v5.json"
-	"settings/navgame2D/MADDPG/HRL_Ant_TRPO_Tensorflow_NoViz_HLC-v4.json"
+#	"settings/navgame2D/MADDPG/HRL_Ant_TRPO_Tensorflow_NoViz_HLC-v4.json"
 #	"settings/navgame2D/MADDPG/HRL_TRPO_Tensorflow_NoViz_HLC-v4.json"
 # 	"settings/navgame2D/MADDPG/HRL_Centralized_Tensorflow_NoViz_HLC-v5.json"
-	"settings/navgame2D/MADDPG/HRL_Ant_Tensorflow_NoViz_HLC-v5.json"
-	"settings/navgame2D/DDPG/HRL_Ant_Tensorflow_NoViz_HLC-v5.json"
-	"settings/navgame2D/TRPO/HRL_Ant_Tensorflow_NoViz_HLC-v5.json"
+#	"settings/navgame2D/MADDPG/HRL_Ant_Tensorflow_NoViz_HLC-v5.json"
+#	"settings/navgame2D/DDPG/HRL_Ant_Tensorflow_NoViz_HLC-v5.json"
+#	"settings/navgame2D/TRPO/HRL_Ant_Tensorflow_NoViz_HLC-v5.json"
+	
+	"settings/navgame2D/DDPG/HRL_Ant_PoseGoal_Tensorflow_NoViz_HLP-v5.json"
+	"settings/navgame2D/TRPO/HRL_Ant_PoseGoal_Tensorflow_NoViz_HLP-v5.json"
 
 )
 
@@ -64,14 +68,14 @@ do
 		# or do whatever with individual element of the array
 		# echo "$simConfigFile"
 		output=' | tee -a $BORGY_JOB_ID.out'
-	 	arg="docker run -e TERRAINRL_PATH=/opt/TerrainRLSim/ -e RLSIMENV_PATH=/opt/RLSimulationEnvironments --rm -it us.gcr.io/glen-rl-framework/glen:latest2 bash -c 'pushd /opt/RL-Framework; ./update_and_compile.sh; backup_data_continuously.sh & python3 tuneHyperParameters.py --config=${simConfigFile} --shouldRender=false --metaConfig=${metaConfig} --meta_sim_sample=3 --meta_sim_threads=3 --tuning_threads=2 ${opts}'"
+	 	arg="docker run -e TERRAINRL_PATH=/opt/TerrainRLSim/ -e RLSIMENV_PATH=/opt/RLSimulationEnvironments --rm -it us.gcr.io/glen-rl-framework/glen:latest2 bash -c 'pushd /opt/RL-Framework; ./update_and_compile.sh; backup_data_continuously.sh & python3 tuneHyperParameters.py --config=${simConfigFile} --shouldRender=false --metaConfig=${metaConfig} --meta_sim_sample=3 --meta_sim_threads=3 --tuning_threads=2 --email_log_data_periodically=true --email_logging_time=28800 ${opts}'"
 	### GPU training
 # 	 	arg="source ~/tensorflow/bin/activate; pushd /home/glen/playground/RL-Framework; python3 tuneHyperParameters.py --config=${simConfigFile} --metaConfig=${metaConfig} --meta_sim_samples=3 --meta_sim_threads=3 --tuning_threads=2 --num_rounds=${rounds} --plot=false --on_policy=fast --shouldRender=false --save_experience_memory=continual --continue_training=last --saving_update_freq_num_rounds=1 -p 4 --rollouts=16 --simulation_timeout=1200 --email_log_data_periodically=true --save_video_to_file=eval_movie2.mp4 --visualize_expected_value=false --force_sim_net_to_cpu=true ${opts}"
 		arg=$arg$output
-		command=(ray exec settings/hyperParamTuning/bair/RL-Framework_Cluster.yaml "$arg")
+		command=(ray exec settings/hyperParamTuning/bair/RL-Framework_Cluster.yaml "$arg" --tmux)
 		echo "${command[@]}"
 		# eval $command
-		# "${command[@]}"
+		"${command[@]}"
 	done
 done
 
