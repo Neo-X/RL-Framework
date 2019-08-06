b7bf80d838e0d29719574dad69769a49f915bebe
Neo-X <glen@fracturedplane.com>
2019-08-06 11:59:33 -0700

    WOrking on docker setup

---
+++ docker/setupDocker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+###
+### Print list of resouce links so you can quickly look through them
+###
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+apt-cache policy docker-ce
+
+sudo apt-get install -y docker-ce
+
+sudo systemctl status docker
+
+sudo usermod -aG docker ${USER}
+
+su - ${USER}
+
+id -nG
+
+### Setup GCP authorization
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+### init and login
+gcloud init
