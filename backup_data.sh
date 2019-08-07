9cbfe0be7e2977212ed7551ab4c8e3a4aa6be513
Neo-X <glen@fracturedplane.com>
2019-08-07 15:43:47 -0700

    WOrking on learning sims

--- backup_data.sh
+++ backup_data.sh
@@ -4,3 +4,4 @@ echo "rsyncing data"
 # gsutil -m rsync -r ./ gs://rl-framework-cluster-bucket/playground/RL-Framework/
 gsutil -m rsync -r ./terrainRLSim/ gs://rl-framework-cluster-bucket/playground/RL-Framework/terrainRLSim/
 gsutil -m rsync -r ./RLSimulations/ gs://rl-framework-cluster-bucket/playground/RL-Framework/RLSimulations/
+gsutil -m rsync -r ./GymMultiChar/ gs://rl-framework-cluster-bucket/playground/RL-Framework/GymMultiChar/
