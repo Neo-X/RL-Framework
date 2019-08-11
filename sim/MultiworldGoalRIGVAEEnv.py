2e4d5b44d7861cff1481110d853c09a2e573d7d9
Brandon Trabucco <brandon@btrabucco.com>
2019-08-10 17:34:18 -0700

    working on RIG

--- sim/MultiworldGoalRIGVAEEnv.py
+++ sim/MultiworldGoalRIGVAEEnv.py
@@ -21,25 +21,31 @@ class MultiworldGoalRIGVAEEnv(MultiworldVAEEnv):
             -1.0 * np.ones([2 * settings["encoding_vector_size"]]),
             1.0 * np.ones([2 * settings["encoding_vector_size"]]))
 
+    def sample_goal_from_prior(self):
+        if self.model is None:
+            self._goal = np.zeros([self.getSettings()["fd_num_terrain_features"]])
+            self._goal_image = np.zeros(self.getSettings()["fd_terrain_shape"])
+        else:
+            noise, image = self.model._sample_latent_variable_image_from_prior([])
+            self._goal = noise[0]
+            self._goal_image = image[0]
+
     def reset(self):
         super(MultiworldGoalRIGVAEEnv, self).reset()
-        self._goal = np.random.normal(0, 1, [self.getSettings()["encoding_vector_size"]])
-        self._goal_image = self.decode(self._goal)
+        self.sample_goal_from_prior()
         self._previous_observation = np.concatenate([self._previous_observation, self._goal], -1)
         self._timestep = 0
         return self._previous_observation
 
     def init(self):
         super(MultiworldGoalRIGVAEEnv, self).reset()
-        self._goal = np.random.normal(0, 1, [self.getSettings()["encoding_vector_size"]])
-        self._goal_image = self.decode(self._goal)
+        self.sample_goal_from_prior()
         self._previous_observation = np.concatenate([self._previous_observation, self._goal], -1)
         self._timestep = 0
             
     def initEpoch(self):
         super(MultiworldGoalRIGVAEEnv, self).reset()
-        self._goal = np.random.normal(0, 1, [self.getSettings()["encoding_vector_size"]])
-        self._goal_image = self.decode(self._goal)
+        self.sample_goal_from_prior()
         self._previous_observation = np.concatenate([self._previous_observation, self._goal], -1)
         self._timestep = 0
         
@@ -48,7 +54,7 @@ class MultiworldGoalRIGVAEEnv(MultiworldVAEEnv):
             x = np.reshape(self._goal_image, self.getSettings()["fd_terrain_shape"])
             x = np.flip(x, 0)
             x = np.flip(x, 2)
-            cv2.imshow("goal image", x)
+            cv2.imshow("goal image", x * 0.5 + 0.5)
         self._timestep = self._timestep + 1
         super(MultiworldGoalRIGVAEEnv, self).step(action)
         reward = -np.sqrt(np.square(self._previous_observation - self._goal).sum())
@@ -56,7 +62,6 @@ class MultiworldGoalRIGVAEEnv(MultiworldVAEEnv):
         self.__reward = np.array([[reward]])
         if self._timestep >= self._skip:
             self._timestep = 0
-            self._goal = np.random.normal(0, 1, [self.getSettings()["encoding_vector_size"]])
-            self._goal_image = self.decode(self._goal)
+            self.sample_goal_from_prior()
         self._previous_observation = np.concatenate([self._previous_observation, self._goal], -1)
         return self.__reward
