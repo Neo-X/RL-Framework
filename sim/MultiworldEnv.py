72be20883417afbb527c08b9f3af3b99e555f8af
Brandon Trabucco <brandon@btrabucco.com>
2019-09-21 16:53:16 -0700

    fixed problems with running sims from multiworld

--- sim/MultiworldEnv.py
+++ sim/MultiworldEnv.py
@@ -9,16 +9,17 @@ class MultiworldEnv(OpenAIGymEnv):
                  image_key="image_observation", state_key="state_observation"):
         # ------------------------------------------------------------
         # set up initial state
-        OpenAIGymEnv.__init__(self, exp, settings, multiAgent=multiAgent)
         self._image_key = image_key
         self._state_key = state_key
         self._previous_image = None
-        assert self._state_key in self.getEnvironment().observation_space.spaces
-        self.observation_space = self.getEnvironment().observation_space.spaces[self._state_key]
+        assert self._state_key in exp.observation_space.spaces
+        self.observation_space = exp.observation_space.spaces[self._state_key]
+        self.action_space = exp.action_space
+        OpenAIGymEnv.__init__(self, exp, settings, multiAgent=multiAgent)
 
     def reset(self):
-        # self.getEnvironment().init()
-        state_dict = self.getEnvironment().reset()
+        # self._exp.init()
+        state_dict = self._exp.reset()
         self._previous_dict = state_dict
         self._previous_observation = state_dict[self._state_key]
         if self._image_key in state_dict:
@@ -28,8 +29,8 @@ class MultiworldEnv(OpenAIGymEnv):
         return self._previous_observation
 
     def init(self):
-        # self.getEnvironment().init()
-        state_dict = self.getEnvironment().reset()
+        # self._exp.init()
+        state_dict = self._exp.reset()
         self._previous_dict = state_dict
         self._previous_observation = state_dict[self._state_key]
         if self._image_key in state_dict:
@@ -38,7 +39,7 @@ class MultiworldEnv(OpenAIGymEnv):
         self._fallen = [False]
 
     def initEpoch(self):
-        state_dict = self.getEnvironment().reset()
+        state_dict = self._exp.reset()
         self._previous_dict = state_dict
         self._previous_observation = state_dict[self._state_key]
         if self._image_key in state_dict:
@@ -49,11 +50,11 @@ class MultiworldEnv(OpenAIGymEnv):
     def step(self, action):
         action_ = np.array(action)
         if (self.getSettings()['render']):
-            self.getEnvironment().render()
+            self._exp.render()
         if (self._multiAgent):
-            observation, reward, done, info = self.getEnvironment().step(action_)
+            observation, reward, done, info = self._exp.step(action_)
         else:
-            observation, reward, done, info = self.getEnvironment().step(action_[0])
+            observation, reward, done, info = self._exp.step(action_[0])
         self._end_of_episode = done
         # self._fallen = done
         self._previous_dict = observation
