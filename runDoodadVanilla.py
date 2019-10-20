61738383e0600c020e69f4ee9132c34835a88106
Brandon Trabucco <brandon@btrabucco.com>
2019-10-19 19:07:21 -0700

    added a plain launcher on Doodad

--- runDoodadVanilla.py
+++ runDoodadVanilla.py
@@ -1,55 +1,40 @@
 from doodad.launch import launch_api
 from doodad import mode
 from doodad import mount
-import sys
-
-cmd = sys.argv[1]
-print ("cmd: ", cmd)
-
-"""
-docker run --rm -it us.gcr.io/glen-rl-framework/glen:latest2 bash -c 'pushd /opt/RL-Framework; ./update_and_compile.sh; python3 trainModel.py --config=tests/settings/gapGame2D/PPO/FixedSTD_Tensorflow-v2.json -p 2 --metaConfig=settings/hyperParamTuning/element/exploration_rate.json --plot=false --shouldRender=false --bootstrap_sample=1 --experiment_logging="{\"use_comet\": true, \"project_name\": \"vizimitation\"}"'
-
-python3 runDoodad.py 'pushd /opt/RL-Framework; ./update_and_compile.sh; python3 trainModel.py --config=tests/settings/gapGame2D/PPO/FixedSTD_Tensorflow-v2.json -p 2 --metaConfig=settings/hyperParamTuning/element/exploration_rate.json --plot=false --shouldRender=false --bootstrap_sample=1'
-
-gcloud compute instances create [INSTANCE_NAME] \
-    --image rl-framework-image \
-    --image-project glen-rl-framework \
-    --machine-type=n1-standard-4 \
-    --boot-disk-size=50GB \
-    --preemptible
+import os
 
 
-"""
+if __name__ == "__main__":
 
-useGCP = True
-if (useGCP):
-    mode = mode.GCPMode(
-            gcp_bucket='rl-framework-cluster-bucket',
-            gcp_log_path='/doodad/logs/',
-            gcp_project='glen-rl-framework',
-            gcp_image='rl-framework-image',
-            gcp_image_project='glen-rl-framework',
-            instance_type='n1-standard-4',
-            terminate_on_end=False,
-            zone='us-west1-a'
-        )
-    mnt = mount.MountGCP(
-        gcp_path='./doodad',
-        mount_point='/opt/doodad',  # Directory visible to the running job.
-        output=True)
+    local_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "doodad_mount")
+    remote_code_dir = "remote_mount"
+    target_py_file = os.path.join(remote_code_dir, "sleep_five_minutes.py")
+    doodad_command = "python {}".format(target_py_file)
 
-else:
-    mode = mode.LocalMode()
-    mnt = mount.MountLocal(
-        local_dir='/home/btrabucco/research/doodad',
-        mount_point='/opt/doodad',
-        output=True)
+    print(dict(
+        local_dir=local_dir,
+        remote_code_dir=remote_code_dir,
+        target_py_file=target_py_file,
+        doodad_command=doodad_command))
 
-out = launch_api.run_command(
-    command=cmd,
-    docker_image='us.gcr.io/glen-rl-framework/glen:latest2',
-    mode=mode,
-    mounts=[mnt]
-)
+    local_mode = mode.LocalMode()
+    gcp_mode = mode.GCPMode(
+        gcp_project='glen-rl-framework',
+        gcp_bucket='rl-framework-cluster-bucket',
+        gcp_log_path='gcp_test',
+        gcp_image='ubuntu-1604-xenial-v20191010',
+        gcp_image_project='ubuntu-os-cloud',
+        instance_type='n1-standard-4',
+        terminate_on_end=True,
+        zone='us-west1-a')
 
-print (out)
+    # replicate the folder structure of the local directory
+    local_mount = mount.MountLocal(
+        local_dir,
+        mount_point=remote_code_dir,
+        output=False)
+
+    print(launch_api.run_command(
+        command=doodad_command,
+        mode=gcp_mode,
+        mounts=(local_mount,)))
