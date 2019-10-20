ddadeb6d848b2dd2faf28643d17423178ee90520
Brandon Trabucco <brandon@btrabucco.com>
2019-10-19 20:15:30 -0700

    added inspect args

--- inspect_gcp_bucket.py
+++ inspect_gcp_bucket.py
@@ -1,14 +1,21 @@
 from google.cloud import storage
+import argparse
 
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--logdir", type=str, default="/doodad/logs2/")
+    parser.add_argument("--bucket", type=str, default="rl-framework-cluster-bucket")
+    args = parser.parse_args()
+
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket("rl-framework-cluster-bucket")
+    bucket = storage_client.get_bucket(args.bucket)
 
     blobs = list(bucket.list_blobs(prefix=""))
     blobs = {x.name: x for x in blobs}
 
-    candidates = [x for x in blobs if "/doodad/logs/" in x]
-    a = blobs[candidates[-1]]
-    print(a.download_as_string().decode("utf-8"))
+    candidates = [x for x in blobs if args.logdir in x]
+    for c in candidates:
+        print(blobs[c].download_as_string().decode("utf-8"))
+        input("press enter to continue...")
