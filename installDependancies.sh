9b3124d52bb383d90c3c80917613136da86814b8
Neo-X <glen@fracturedplane.com>
2019-12-05 11:21:13 -0800

    Updating dependancies

--- installDependancies.sh
+++ installDependancies.sh
@@ -2,7 +2,7 @@
 # do not run this with sudo 
 # Maybe just run the first one with sudo
 
-apt-get -y install python3-dev python3-pip
+# apt-get -y install python3-dev python3-pip
 
 pip3 install --user numpy
 pip3 install --user theano==1.0.1
@@ -29,4 +29,4 @@ pip3 install --user pydot
 pip3 install --user pillow
 
 ### For rendering interactive metplotlib figures during training
-apt-get -y install python3-tk
+# apt-get -y install python3-tk
