06019c3a81ea38572373669ee1c43fe7be683683
Neo-X <glen@fracturedplane.com>
2020-02-01 20:47:37 -0800

    Working on tag observation size

--- sim/OpenAIGymEnv.py
+++ sim/OpenAIGymEnv.py
@@ -102,7 +102,9 @@ class OpenAIGymEnv(SimInterface):
         # observation, reward, done, info = env.step(action)
         # self._previous_observation = observation
         
-        state = [np.array(self._previous_observation)]
+        # state = [np.array(self._previous_observation)]
+        ### This "Fix" Might break a lot of other code...
+        state = self._previous_observation
         """
         ### Because some of the state parameters from the sim are always the same number.
         state_idx=0
