13bb107ed59ddc7b296f3342202e66727a114a3d
Neo-X <glen@fracturedplane.com>
2020-03-19 12:21:45 -0700

    Working on SLAC

---
+++ settings/hyperParamTuning/bair/test_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=co_rail
+#SBATCH --mem=16384M
+#SBATCH --time=8:00:00
+#SBATCH --job-name=test
+#SBATCH --output=%x-%j.out
+#SBATCH --cpus-per-task=8
+
+#SBATCH --mail-user=gberseth@berkeley.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+# The arguments should always start with the keyword SBATCH
+# The arguments must immediately follow the first line (i.e. `!/bin/bash`)
+$1  # use your command here
