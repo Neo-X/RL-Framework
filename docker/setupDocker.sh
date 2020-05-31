b9dab246d87f3f5dc010637e40a1cdc081fa5561
Neo-X <glen@fracturedplane.com>
2020-05-31 12:32:12 -0700

    Switching back to eval

--- docker/setupDocker.sh
+++ docker/setupDocker.sh
@@ -3,6 +3,8 @@
 ### Print list of resouce links so you can quickly look through them
 ###
 
+sudo apt-get -y install curl
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
