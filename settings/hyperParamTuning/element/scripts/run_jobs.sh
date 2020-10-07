696554d683ad892b986639e6b7160fdef981bd20
Neo-X <glen@fracturedplane.com>
2020-10-06 17:10:24 -0700

    Working on TD3

--- settings/hyperParamTuning/element/scripts/run_jobs.sh
+++ settings/hyperParamTuning/element/scripts/run_jobs.sh
@@ -9,7 +9,7 @@ export ACCOUNT_ID=$ORG_NAME.$ACCOUNT_NAME
 eai data push $ORG_NAME.$ACCOUNT_NAME.rlframe /home/gberseth/playground/RL-Framework/
 eai data push $ORG_NAME.$ACCOUNT_NAME.doodad /home/gberseth/playground/doodad_vitchry/
 eai data push $ORG_NAME.$ACCOUNT_NAME.motionimitation /home/gberseth/playground/motion_imitation/
-eai data push $ORG_NAME.$ACCOUNT_NAME.stanquad /home/gberseth/playground/StanfordQuadruped/
+# eai data push $ORG_NAME.$ACCOUNT_NAME.stanquad /home/gberseth/playground/StanfordQuadruped/
 # eai data push $ORG_NAME.$ACCOUNT_NAME.learning_data /home/gberseth/learning_data_eai/
 
 for i in `seq 1 3`;
@@ -21,14 +21,14 @@ for i in `seq 1 3`;
 	    --data $ORG_NAME.$ACCOUNT_NAME.stanquad:/home/gberseth/playground/StanfordQuadruped/ \
 	    --data $ORG_NAME.$ACCOUNT_NAME.learning_data:/home/gberseth/learning_data/ \
 	    --image gberseth/rlframe:latest \
-	    -e PYTHONPATH=/home/gberseth/playground/RL-Framework/:/home/gberseth/playground/doodad/:/home/gberseth/playground/motion_imitation/motion_imitation/:/home/gberseth/playground/StanfordQuadruped/ \
-	    --gpu 1 --cpu 5 --mem 16 \
-	    -- bash -c "pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_StanQuad_1Sub_LSTM_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=hyper_train --random_seed=$i"
-	    # -- bash -c "pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree_1Sub_LSTM_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh2.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train --random_seed=$i"
+	    -e PYTHONPATH=/home/gberseth/playground/RL-Framework/:/opt/TerrainRLSim:/home/gberseth/playground/doodad/:/home/gberseth/playground/motion_imitation/motion_imitation/:/home/gberseth/playground/StanfordQuadruped/ \
+	    --gpu 0 --cpu 5 --mem 16 \
+	    -- bash -c "pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/TD3/Imitation_Learning_GRF_Dog.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=hyper_train --random_seed=$i"
+	    # -- bash -c "pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree_1Sub_LSTM_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh2.json -p 8 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train --random_seed=$i"
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_StanQuad.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'
 	    # --gpu 1 --cpu 5 --mem 16 --name test_eai_terrainrl_laikago_ppo1 \
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree.json -p 5 --shouldRender=false --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/TD3/Imitation_Learning_GRF_UniTree.json -p 6 --shouldRender=false --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree_1Sub_LSTM_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh2.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'    
-            echo $i
+        echo $i
     done 
