69c4b0f9c2c70ceee0075a8efa610c966cc90c95
Neo-X <glen@fracturedplane.com>
2020-11-19 11:00:26 -0800

    Working on getting data

---
+++ copy_data_off_aws.sh
@@ -0,0 +1,2 @@
+
+aws s3 sync s3://comps-test/rlframe/ ~/learning_data/rlframe/
--- settings/hyperParamTuning/element/scripts/run_jobs.sh
+++ settings/hyperParamTuning/element/scripts/run_jobs.sh
@@ -6,17 +6,26 @@ export ORG_NAME=$(eai organization  get --fields name --no-header)
 export ACCOUNT_NAME=$(eai account get --fields name --no-header)
 export ACCOUNT_ID=$ORG_NAME.$ACCOUNT_NAME
 
-# eai data push $ORG_NAME.$ACCOUNT_NAME.rlframe /home/gberseth/playground/RL-Framework/
+eai data push $ORG_NAME.$ACCOUNT_NAME.rlframe /home/gberseth/playground/RL-Framework/
 # eai data push $ORG_NAME.$ACCOUNT_NAME.doodad /home/gberseth/playground/doodad_vitchry/
 # eai data push $ORG_NAME.$ACCOUNT_NAME.motionimitation /home/gberseth/playground/motion_imitation/
 # eai data push $ORG_NAME.$ACCOUNT_NAME.stanquad /home/gberseth/playground/StanfordQuadruped/
 # eai data push $ORG_NAME.$ACCOUNT_NAME.learning_data /home/gberseth/learning_data_eai/
 
-# for simConfigFile in "${simConfigs[@]}"
-# do
+## declare an array variable
+declare -a simConfigs=(
+ 	"settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree_1Sub_LSTM_Critic_And_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh3.json"
+	"settings/terrainRLImitate/PPO/Imitation_Learning_GRF_StanQuad_LSTM_Critic_And_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh.json"
+#
+)
+
+opts=' -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=hyper_train'
+for simConfigFile in "${simConfigs[@]}"
+do
 	for i in `seq 1 2`;
 	    do
-		eai job submit --preemptable\
+	    arg="pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=${simConfigFile}  --random_seed=$i ${opts}"
+		command=(eai job submit --preemptable\
 	    --data $ORG_NAME.$ACCOUNT_NAME.rlframe:/home/gberseth/playground/RL-Framework/ \
 	    --data $ORG_NAME.$ACCOUNT_NAME.doodad:/home/gberseth/playground/doodad/ \
 	    --data $ORG_NAME.$ACCOUNT_NAME.motionimitation:/home/gberseth/playground/motion_imitation/ \
@@ -25,7 +34,7 @@ export ACCOUNT_ID=$ORG_NAME.$ACCOUNT_NAME
 	    --image gberseth/rlframe:latest \
 	    -e PYTHONPATH=/home/gberseth/playground/RL-Framework/:/opt/TerrainRLSim:/home/gberseth/playground/doodad/:/home/gberseth/playground/motion_imitation/motion_imitation/:/home/gberseth/playground/StanfordQuadruped/ \
 	    --gpu 1 --cpu 5 --mem 16 \
-	    -- bash -c "pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_StanQuad_LSTM_Critic_And_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train --random_seed=$i"
+	    -- bash -c "$arg")
 	    # -- bash -c "pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree_1Sub_LSTM_Critic_And_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh3.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train --random_seed=$i"
 	    # -- bash -c "pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/TD3/Imitation_Learning_GRF_Humanoid2D.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=hyper_train --random_seed=$i"
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_StanQuad.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'
@@ -33,6 +42,8 @@ export ACCOUNT_ID=$ORG_NAME.$ACCOUNT_NAME
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree.json -p 5 --shouldRender=false --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/TD3/Imitation_Learning_GRF_UniTree.json -p 6 --shouldRender=false --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'
 	    # -- bash -c 'pushd /home/gberseth/playground/RL-Framework/; python3 trainModel.py --config=settings/terrainRLImitate/PPO/settings/terrainRLImitate/PPO/Imitation_Learning_GRF_UniTree_1Sub_LSTM_FD_Reward_Dual_Encode_Decode_VAE_2State_2_Advisarial_BCE_refresh2.json -p 5 --shouldRender=false --log_comet=true --on_policy=fast --print_level=train'    
+        echo "${command[@]}"
+ 		"${command[@]}"
         echo $i
     done
-# done
+done
